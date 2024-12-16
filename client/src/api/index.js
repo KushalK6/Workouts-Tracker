669a83a939c8957b8923c227f610e31856130693
Kushal <kushalkollu.6@gmail.com>
2024-12-16 19:02:46 +0530

    dotenv improvements

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 // client\src\api\index.js
 import axios from "axios";
-
+import * as dotenv from "dotenv"; 
+dotenv.config()
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
