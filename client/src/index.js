6dfd83ef03ea4196a6f7857e21a93928eca9c4bc
Kushal <kushalkollu.6@gmail.com>
2024-12-16 19:05:25 +0530

    dotenv improvements 2.0

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-
+import * as dotenv from "dotenv"; 
+dotenv.config()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
